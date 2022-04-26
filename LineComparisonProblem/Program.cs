e6a4630cdb7aa9bf05db889e9f6e121515dac948
nk321626 <nk321626@gmail.com>
2022-04-27 01:33:19 +0530

    checked equality

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -4,3 +4,6 @@ int x1 = 4, y1 = 5, x2 = 6, y2 = 7;
 double length = 0;
 length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 Console.WriteLine("length of the line = " + length);
+int a = 0;
+int b = 0; 
+Console.WriteLine( a == b );
