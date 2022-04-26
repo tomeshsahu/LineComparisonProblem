f0699e5c8f163905ee2bdd1c66892704b1a031e8
nk321626 <nk321626@gmail.com>
2022-04-27 00:14:37 +0530

    Calculated length

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Line Comparison Computation Program");
+int x1 = 4, y1 = 5, x2 = 6, y2 = 7;
+double length = 0;
+length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine("length of the line = " + length);
